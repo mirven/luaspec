678c0de77f95ea1a4d7304b2a10dd6c0da4d5622
Marcus Irven <marcus@marcusirven.com>
2008-12-29 15:29:04 -0600

    Rewrite luamock test first

--- specs/luamock_spec.lua
+++ specs/luamock_spec.lua
@@ -1,91 +1,86 @@
 require 'luaspec'
 require 'luamock'
 
-describe["MockAccessor"] = function()
-	it["can be created with a name and mock object"] = function()
-		m = MockAccessor:new("a_name", {})
-		expect(m).should_not_be(nil)
-	end
-end
-
-describe["a newly created MockAccessor"] = function()
-
-	before = function()
-		name = "a_name"
-		mock = {}
-		m = MockAccessor:new(name, mock)
-	end
-	
-	it["should set mock property"] = function()
-		expect(m.mock).should_be(mock)
-	end
-
-	it["should set name property"] = function()
-		expect(m.name).should_be(name)
-	end
-	
-	it["when called as a function it should add an entry to the mock.__calls[name] array containing the parameters"] = function()
-		mock.__calls = {}
-		m(37)
-		expect(#mock.__calls[name]).should_be(1)
-		expect(#mock.__calls[name][1]).should_be(1)
-		expect(mock.__calls[name][1][1]).should_be(37)
-	end
-end
-
-describe["Mock"] = function()
-	it["can be created"] = function()
-		mock = Mock:new()
-		expect(mock).should_not_be(nil)
-	end
-end
-
-describe["a newly created Mock" ] = function()
+describe["a Mock"] = function()
 	before = function()
 		mock = Mock:new()
 	end
 	
-	it["should have a __calls table"] = function()
-		expect(type(mock.__calls)).should_be("table")
+	describe["when called as a function with no parameters"] = function()
+		before = function()
+			return_value = mock()
+		end
+		
+		it["should return nil"] = function()
+			expect(return_value).should_be(nil)
+		end
+
+		it["should record the call in Mock.calls"] = function()
+			expect(#Mock.calls[mock]).should_be(1)
+		end
+		
+		it["should record the parameters (or lack there of)"] = function()
+			expect(#Mock.calls[mock][1]).should_be(0)
+		end
+		
+		describe["when called a second time with a few parameters"] = function()
+			before = function()
+				mock(1, "two", 3)
+			end
+
+			it["should record the call in Mock.calls"] = function()
+				expect(#Mock.calls[mock]).should_be(2)
+			end
+
+			it["should record the parameters"] = function()
+				-- TODO, replace with a single matcher that compares an array
+				-- e.g. expect(Mock.calls[mock][2]).should_be({ 1, "two", 3 })
+				expect(#Mock.calls[mock][2]).should_be(3)
+				expect(Mock.calls[mock][2][1]).should_be(1)
+				expect(Mock.calls[mock][2][2]).should_be("two")
+				expect(Mock.calls[mock][2][3]).should_be(3)
+			end
+		end
 	end
+	
+	describe["when accessing a new property"] = function()
+		before = function()
+			field = mock.foo
+		end
+		
+		it["should create a new Mock instance"] = function()
+			expect(getmetatable(field)).should_be(Mock)
+		end
+	end
+	
+	describe["when setting a property"] = function()
+		before = function()
+			mock.foo = 25
+		end
+		
+		it["should be accessible"] = function()
+			expect(mock.foo).should_be(25)
+		end
+	end
+	
+	describe["when specifying a return value"] = function()
+		before = function()
+			mock:should_return(10)
+		end
+		
+		it["should return that value when called as a function"] = function()
+			expect(mock()).should_be(10)
+		end
+	end
+
+	describe["when specifying a return value using . instead of :"] = function()
+		before = function()
+			err = track_error(function() mock.should_return(10) end)
+		end
+		
+		it["should produce an error"] = function()
+			expect(err).should_not_be(nil)
+		end
+	end
+	
 end
-
-
-describe["a Mock after mock.foo has been called"] = function()
-	before = function()
-		mock = Mock:new()
-		mock.foo()
-	end
-	
-	it["should have a __calls table with a foo entry"] = function()
-		expect(type(mock.__calls.foo)).should_be("table")
-	end
-	
-	it["should have a __calls table with a foo array with one entry"] = function()
-		expect(#mock.__calls.foo).should_be(1)
-	end
-end
-
-describe["was_called matcher"] = function()
-	before = function()
-		mock = Mock:new()
-		mock.foo()
-	end
-	
-	it["should return true when called with mock.foo, 1"] = function()
-		expect(matchers.was_called(mock.foo, 1)).should_be(true)
-	end
-	
-	it["should return false when called with mock.bar, 1"] = function()
-		expect(matchers.was_called(mock.bar, 1)).should_be(false)
-	end
-
-	it["should work as a matcher"] = function()
-		expect(mock.foo).was_called(1)
-	end
-
-	it["should return false when something other than a MockAccessory is passed in"] = function()
-		expect(matchers.was_called({}, 1)).should_be(false)
-	end		
-end
-
--- src/luamock.lua
+++ src/luamock.lua
@@ -1,41 +1,56 @@
-MockAccessor = {}
-MockAccessor.__index = MockAccessor
+Mock = { calls = {}, return_values = {} }
 
-function MockAccessor.__call(t, ...)
-	t.mock.__calls[t.name] = t.mock.__calls[t.name] or {}
-	local call_list = t.mock.__calls[t.name]
-	call_list[#call_list+1] = {...}
+-- we store all calls and requested return values indexed
+-- by the mock object, we don't want to hold onto references
+-- when they should be garbage collected to make the tables
+-- be weak
+setmetatable(Mock.calls, { __mode="k" })
+setmetatable(Mock.return_values, { __mode="k" })
+
+function Mock.__call(mock, ...)
+	Mock.calls[mock] = Mock.calls[mock] or {}
+	local calls = Mock.calls[mock]
+	calls[#calls+1] = {...}
+	
+	local return_values = Mock.return_values[mock]
+	
+	if return_values then
+		return unpack(return_values)
+	end
 end
 
-function MockAccessor:new(name, mock)
-	local o = { name = name, mock = mock }
-	setmetatable(o, self)
-	return o
-end
-
-Mock = {}
-
-function Mock.__index(t,k)
-	return MockAccessor:new(k, t)
+function Mock.__index(mock, k)
+	local new_mock = Mock:new()
+	rawset(mock, k, new_mock)
+	return new_mock
 end
 
 function Mock:new()
-	local o = { __calls = {} }
-	setmetatable(o, self)
-	return o
+	local mock = { should_return = self.should_return }
+	setmetatable(mock, self)
+	return mock
 end
+
+function Mock:should_return(...)
+	if getmetatable(self) ~= Mock then
+		error("should_return must be called with : operator", 2)
+	end
+	Mock.return_values[self] = {...}
+end
+
+-- define matchers used with mocks
 
 matchers = matchers or {}
 
 function matchers.was_called(target, value)
-	if getmetatable(target) ~= MockAccessor then
-		return false, "target must a MockAccessor"
+	if getmetatable(target) ~= Mock then
+		return false, "target must be a Mock"
 	end
 	
-	local calls = target.mock.__calls[target.name]
+	local calls = Mock.calls[target] or {}
 	
-	if not calls or #calls ~= value then
-		return false, "expecting "..value.." calls"
+	if #calls ~= value then
+		return false, "expecting "..value.." calls, actually "..#calls
 	end
 	return true
 end
